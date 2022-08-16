d382edac5e928f8210b874ca0318f5cd8f4702f1
Alvaro-Siqueira-Silva <84384180+Alvaro-Siqueira-Silva@users.noreply.github.com>
2022-08-16 13:05:01 -0300

    Add files via upload

---
+++ routes/cadastrar.js
@@ -0,0 +1,22 @@
+module.exports = (app)=>{
+    
+    app.post('/cadastrar', async(req,res)=>{
+        var dados = req.body
+
+        const database = require('../config/database')()
+
+        const produto = require('../models/produto')
+
+        var gravar = await new produto({
+            data:dados.data,
+            produto:dados.produto
+        }).save()
+
+        res.redirect('/cadastrar')
+    })
+
+    app.get('/cadastrar',(req,res)=>{
+        res.render('cadastrar.ejs')
+    })
+
+    }
